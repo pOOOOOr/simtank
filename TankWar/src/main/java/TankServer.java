e371693b03dd162d393097700cd448789535d3b4
pOOOOOr <au.huangxin@gmail.com>
2015-04-29 16:32:14 +1000

    server heartbeat message

--- TankWar/src/main/java/TankServer.java
+++ TankWar/src/main/java/TankServer.java
@@ -12,18 +12,58 @@ public class TankServer {
     public static final int UDP_PORT = 6666;
     private static int INIT_ID = 100;
     private List<Client> clients = new ArrayList<>();
+    private List<Socket> sockets = new ArrayList<>();
 
-    public static void main(String[] args) {
-        try {
-            new TankServer().start();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        TankServer tankServer = new TankServer();
+        tankServer.start();
+    }
+
+    private String generateClientsMessage() {
+        StringBuilder message = new StringBuilder();
+
+        for (Client c : clients) {
+            message.append(c.IP).append("|");
+        }
+        message.append("\n");
+
+        return message.toString();
+    }
+
+    /*
+    * send client table
+    * health check
+    * */
+    private class Beat implements Runnable {
+        @Override
+        public void run() {
+            DataInputStream inputStream;
+            DataOutputStream outputStream;
+
+            while (true) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                for (Socket s : sockets) {
+                    try {
+                        inputStream = new DataInputStream(s.getInputStream());
+                        System.out.println(String.format("message from [%s]: %s.", s.getInetAddress(), inputStream.readInt()));
+                        outputStream = new DataOutputStream(s.getOutputStream());
+                        outputStream.writeBytes(generateClientsMessage());
+                    } catch (IOException e) {
+                        System.out.println(s.getInetAddress() + " die");
+                    }
+                }
+            }
         }
     }
 
     public void start() throws IOException {
 
         new Thread(new UDPThread()).start();
+        new Thread(new Beat()).start();
 
         ServerSocket serverSocket = new ServerSocket(TCP_PORT);
 
@@ -34,18 +74,19 @@ public class TankServer {
                 DataInputStream inputStream = new DataInputStream(socket.getInputStream());
                 String IP = socket.getInetAddress().getHostAddress();
                 int udpPort = inputStream.readInt();
-                Client client = new Client(IP, udpPort);
-                clients.add(client);
+
                 DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
                 outputStream.writeInt(INIT_ID++);
+
+                clients.add(new Client(IP, udpPort));
+                sockets.add(socket);
 
                 System.out.println(String.format("Client %s:%s - UDP[%s] connect!",
                         socket.getInetAddress(), socket.getPort(), udpPort));
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
-                if (socket != null)
-                    socket.close();
+                // if (socket != null) socket.close();
             }
         }
     }
